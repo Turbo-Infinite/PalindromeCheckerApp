4a7bc2d561ed8a8f2aa5f4e1c8a90227770dfa56
Mayank Singh Somvanshi <mayanksinghsomvanshi26@gmail.com>
2026-02-27 14:47:00 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String input = sc.next();
+        System.out.print("Is it a palindrome? : ");
+        int n = input.length();
+        boolean pal = true;
+        for(int x=0; x<n/2; x++)
+        {
+            if(input.charAt(x)!=input.charAt(n-1-x))
+            {
+                pal = false;
+                break;
+            }
+        }
+        if(pal==true)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
     }
 }
