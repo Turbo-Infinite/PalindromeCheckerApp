e2bd08d9daa66e4711d08d1f4ac187116ed91916
Mayank Singh Somvanshi <mayanksinghsomvanshi26@gmail.com>
2026-03-09 14:04:55 +0530

    UC9:Recursive Palindrome

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,28 @@ public class Main {
         String input = sc.next();
         System.out.print("Is it a palindrome? : ");
         int n = input.length();
-        LinkedList<Character> list = new LinkedList<>();
-        for(char c : input.toCharArray())
-        {
-            list.add(c);
-        }
-        boolean isPalindrome = true;
-        while(list.size() > 1)
-        {
-            if(list.removeFirst()!=list.removeLast())
-            {
-                isPalindrome = false;
-                break;
-            }
-        }
-        if(isPalindrome==true)
+        if(check(input,0,n-1)==true)
         {
             System.out.println("true");
         }
         else
         {
             System.out.println("false");
+        }
+    }
+    private static boolean check(String s, int start, int end)
+    {
+        if(start==end)
+        {
+            return true;
+        }
+        else if (s.charAt(start)!=s.charAt(end))
+        {
+            return false;
+        }
+        else
+        {
+            return check(s,start+1,end-1);
         }
     }
 }
